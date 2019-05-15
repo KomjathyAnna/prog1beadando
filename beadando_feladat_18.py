735cbfe1426f142666fb032a4a2647b735276832
KomjathyAnna <komjathyanna1999@gmail.com>
2019-05-15 08:09:37 +0200

    hfchf

--- beadando_feladat_18.py
+++ beadando_feladat_18.py
@@ -1,51 +1,32 @@
 from itertools import product, islice
 
 
-def expr(p):
+def szamok(p):
     return "{}1{}2{}3{}4{}5{}6{}7{}8{}9".format(*p)
 
 
-def gen_expr():
-    op = ['+', '-', '']
-    return [expr(p) for p in product(op, repeat=9) if p[0] != '+']
+def generalt():
+    m_jelek = ['+', '-', '']
+    return [szamok(p) for p in product(m_jelek, repeat=9) if p[0] != '+']
 
 
-def all_exprs():
-    values = {}
-    for expr in gen_expr():
-        val = eval(expr)
-        if val not in values:
-            values[val] = 1
+def osszes_szam():
+    ertekek = {}
+    for szamok in generalt():
+        ertek = eval(szamok)
+        if ertek not in ertekek:
+            ertekek[ertek] = 1
         else:
-            values[val] += 1
-    return values
+            ertekek[ertek] += 1
+    return ertekek
 
 
-def sum_to(val):
-    for s in filter(lambda x: x[0] == val, map(lambda x: (eval(x), x), gen_expr())):
+def szamol(ertek):
+    for s in filter(lambda x: x[0] == ertek, map(lambda x: (eval(x), x), generalt())):
         print(s)
 
 
-def max_e():
-    print("Sum {} has the maximum number of solutions: {}".
-          format(*max(all_exprs().items(), key=lambda x: x[1])))
 
 
-def min_e():
-    values = all_exprs()
-    for i in range(123456789):
-        if i not in values:
-            print("Lowest positive sum that can't be expressed: {}".format(i))
-            return
 
-
-def highest_sums(n=10):
-    sums = map(lambda x: x[0],
-               islice(sorted(all_exprs().items(), key=lambda x: x[0], reverse=True), n))
-    print("Highest Sums: {}".format(list(sums)))
-
-
-sum_to(100)
-max_e()
-min_e()
-highest_sums()
+szamol(100)
